55af368d6a24d02bb32b3dfb7bbeef1b5a6920bd
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-07-12 22:24:21 -0400

    assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,18 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require js/jquery-1.9.1.min
+//= require plugins/jflickrfeed/jflickrfeed.min
+//= require plugins/tweet/jquery.tweet.min
+//= require plugins/fitvids/jquery.fitvids.min
+//= require plugins/map/gmap3.min
+//= require plugins/isotope/jquery.isotope.min
+//= require plugins/caroufredsel/jquery.carouFredSel-6.2.0-packed
+//= require plugins/flexslider/jquery.flexslider-min
+//= require js/jquery.timeline.min
+//= require plugins/magnificpopup/jquery.magnific-popup.min
+//= require plugins/layerslider/js/jquery-easing-1.3
+//= require plugins/layerslider/js/jquerytransit
+//= require plugins/layerslider/js/layerslider.transitions
+//= require plugins/layerslider/js/layerslider.kreaturamedia.jquery.min
+//= require js/youxi.plugins.min
+//= require js/youxi.setup
