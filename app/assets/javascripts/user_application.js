0bfc456778cd5aab3e95135a09e1547d60f05981
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-07-13 21:14:44 -0400

    Satrt on users area

---
+++ app/assets/javascripts/user_application.js
@@ -0,0 +1,15 @@
+//= require jquery
+//= require jquery_ujs
+//= require js/jquery-1.9.1.min
+//= require js/libs/jquery-1.8.3.min
+//= require js/libs/jquery.mousewheel.min
+//= require js/libs/jquery.placeholder.min
+//= require custom-plugins/fileinput
+//= require jui/js/jquery-ui-1.9.2.min
+//= require jui/jquery-ui.custom.min
+//= require jui/js/jquery.ui.touch-punch
+//= require plugins/datatables/jquery.dataTables.min
+//= require plugins/colorpicker/colorpicker-min
+//= require bootstrap/js/bootstrap.min
+//= require js/core/mws
+//= require js/demo/demo.table
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,14 +1,14 @@
 class UserController < ApplicationController
-  expose(:current_account){ current_user.account }
-  layout 'user_application'
-  before_filter :check_user_active
-  private
-  def check_user_active
-    unless current_user.active
-      flash[:notice]= t(:user_not_active)
-      sign_out current_user
-      redirect_to new_user_session_path
-    end
-  end
+ expose(:current_account){ current_user.account }
+ layout 'user_application'
+ before_filter :check_user_active
+ private
+ def check_user_active
+   unless current_user.active
+     flash[:notice]= t(:user_not_active)
+     sign_out current_user
+     redirect_to new_user_session_path
+   end
+ end
 end
 
