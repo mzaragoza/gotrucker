414f83b807afb748c719aefd319d05d51b88a608
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-07-20 12:32:30 -0400

    add maps to drivers

--- app/models/broker.rb
+++ app/models/broker.rb
@@ -5,6 +5,7 @@ class Broker < ActiveRecord::Base
   attr_accessible :name, :phone, :address, :address2, :city, :state, :zip, :fax, :email, :website, :motor_carrier_number, :carreir_packet_pdf, :brokers
 
   before_validation { |broker| broker.phone = phone.to_s.gsub(/[^0-9]/, "").to_s }
+  before_validation { |broker| broker.fax = fax.to_s.gsub(/[^0-9]/, "").to_s }
   before_validation { |broker| broker.zip = zip.to_s.gsub(/[^0-9]/, "").to_s }
 
   validates_uniqueness_of :name, :scope => :account_id
