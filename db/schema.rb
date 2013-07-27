5004abb011df5d616fc608cc5208bc6f8ddc4d4b
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-07-26 23:12:17 -0400

    reloase date for drivers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130726211201) do
+ActiveRecord::Schema.define(:version => 20130727030735) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name",                 :default => ""
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(:version => 20130726211201) do
     t.datetime "created_at",                                                             :null => false
     t.datetime "updated_at",                                                             :null => false
     t.string   "medical_long_form_pdf",                               :default => ""
+    t.string   "release_date"
   end
 
   create_table "invoices", :force => true do |t|
