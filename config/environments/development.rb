44734d7eecdb71cb7b52db6f77966147ca9d4dfc
Moises Zaragoza <mZaragoza@circletechfl.com>
2013-07-30 18:08:09 -0400

    testing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Gotrucker::Application.configure do
+  AwesomePrint.irb!
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
@@ -34,7 +35,7 @@ Gotrucker::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :letter_opener
---
+++ features/owner_creates_an_account.feature
@@ -0,0 +1,16 @@
+#@javascript
+Feature: Owner creates an account
+  In order for us to have clients 
+  they should be able to create an account
+
+  Background:
+    Given the following plans:
+    | name | slug | monthy_price | yearly_price | featured | active | licenses |
+    | Free | free | 20           | 200          | true     | true   | 1        |
+
+  Scenario: Owner should be able to create an account
+    Given I go to the home page
+    And I debug
+    When I follow "Try For Free"
+    Then I should see "Pricing Table"
+
